da96a07901b5a623f8d2c0015a9fbde6366d4c5e
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-24 17:36:13 -0500

    fix migration problem

--- db/migrate/20150224194905_devise_create_users.rb
+++ db/migrate/20150224194905_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
-  def change
-    create_table(:models) do |t|
+  def up
+    create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -34,8 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :models, :confirmation_token,   unique: true
     # add_index :models, :unlock_token,         unique: true
   end
