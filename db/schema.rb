e17c91b4c6e1f955561bc4f2586e7c564aa023d5
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-25 16:03:49 -0500

    playlists and songs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224214215) do
+ActiveRecord::Schema.define(version: 20150225135821) do
+
+  create_table "playlists", force: :cascade do |t|
+    t.integer  "votes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
