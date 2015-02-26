f8b34985ddfbdec77e6f8d26ee1b18e6a21a1fcb
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-25 19:51:40 -0500

    add albums

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225215825) do
+ActiveRecord::Schema.define(version: 20150226004915) do
 
   create_table "playlists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150225215825) do
     t.string   "spotify_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "album"
   end
 
   create_table "users", force: :cascade do |t|
